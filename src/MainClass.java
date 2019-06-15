ff93abe4ae176341c1e1176e39aaa4fd2aa2fe63
marynakolesnik <marna.kolesnik@gmail.com>
2019-06-15 14:53:38 +0300

    testGetClassNumber

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,14 @@ public class MainClass {
 
     int localNum = 14;
 
+    private int class_number = 20;
+
     public int getLocalNumber() {
         return this.localNum;
     }
+
+    public int getClassNumber() {
+        return this.class_number;
+    }
+
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class MainClassTest {
@@ -8,6 +9,12 @@ public class MainClassTest {
     @Test
     public void testGetLocalNumber(){
         MainClass main = new MainClass();
-        assertEquals("#testGetLocalNumber should return 14", 14, main.getLocalNumber());
+        assertEquals("#getLocalNumber should return 14", 14, main.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        MainClass main = new MainClass();
+        assertTrue("#getClassNumber should return > 45", main.getClassNumber() > 45);
     }
 }
