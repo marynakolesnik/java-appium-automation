b53be239bac780ecd21299b31abde22b0787d860
marynakolesnik <marna.kolesnik@gmail.com>
2019-06-15 15:16:50 +0300

    testGetClassString

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,12 +4,19 @@ public class MainClass {
 
     private int class_number = 20;
 
+    private String class_string = "Hello, world";
+
+
     public int getLocalNumber() {
         return this.localNum;
     }
 
     public int getClassNumber() {
         return this.class_number;
+    }
+
+    public String getClassString() {
+        return this.class_string;
     }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -15,6 +15,15 @@ public class MainClassTest {
     @Test
     public void testGetClassNumber(){
         MainClass main = new MainClass();
-        assertTrue("#getClassNumber should return > 45", main.getClassNumber() > 45);
+        int actual = main.getClassNumber();
+        assertTrue("#getClassNumber should return > 45, returned " + actual, main.getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString(){
+        MainClass main = new MainClass();
+        String actual = main.getClassString();
+        assertTrue("#getClassString should return 'Hello, world' with substring 'Hello' or 'hello', but returned " + actual,
+                actual.contains("Hello") || actual.contains("hello"));
     }
 }
